c6149673b37a86b4589ae189f1415ccecb2691ad
25021962-PhamMinhQuan <25021962@vnu.edu.vn>
2026-09-15 11:27:33 +0700

    refractor(spark session): Add delta lake functionality to session setup

--- src/transform/spark_session.py
+++ src/transform/spark_session.py
@@ -11,7 +11,15 @@ class F1SparkSession:
             SparkSession.builder
             .appName(app_name)
             .master(master)
-            .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0")  # Allow kafka connection
-            .config("spark.sql.shuffle.partitions", "4")  # Reduce the number of shuffle partitions for local testing
+            .config(
+                "spark.jars.packages",
+                "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,"
+                "io.delta:delta-spark_2.12:3.1.0",
+            )
+            # Required configuration for full Delta Lake functionality
+            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+            # Optimization for local environment
+            .config("spark.sql.shuffle.partitions", "4")
             .getOrCreate()
         )
