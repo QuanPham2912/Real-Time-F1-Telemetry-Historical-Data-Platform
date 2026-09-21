86095c470bd50812f535cd8fa08692e9de20b756
25021962-PhamMinhQuan <25021962@vnu.edu.vn>
2026-09-21 20:00:10 +0700

    refractor(spark session): Add basic of MinIO to spark session

--- src/transform/spark_session.py
+++ src/transform/spark_session.py
@@ -14,12 +14,20 @@ class F1SparkSession:
             .config(
                 "spark.jars.packages",
                 "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,"
-                "io.delta:delta-spark_2.12:3.1.0",
+                "io.delta:delta-spark_2.12:3.1.0,"
+                "org.apache.hadoop:hadoop-aws:3.3.4,"
+                "com.amazonaws:aws-java-sdk-bundle:1.12.262"
             )
             # Required configuration for full Delta Lake functionality
             .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
             .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
             # Optimization for local environment
             .config("spark.sql.shuffle.partitions", "4")
+        # MinIO Connection
+            .config("spark.hadoop.fs.s3a.endpoint", "http://localhost:9000")  # Dùng "http://minio:9000" nếu chạy TRONG Docker
+            .config("spark.hadoop.fs.s3a.access.key", "minioadmin")
+            .config("spark.hadoop.fs.s3a.secret.key", "minioadmin123")
+            .config("spark.hadoop.fs.s3a.path.style.access", "true")
+            .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
             .getOrCreate()
         )
